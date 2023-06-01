3d2dc6323ef05ab4ef0edb0ca6d779509217aab2
ania orwatk <orwat.ania@gmail.com>
2023-06-01 09:08:20 +0200

    created card and var colors

---
+++ components/Card/Card.tsx
@@ -0,0 +1,28 @@
+import styles from "./Card.module.css";
+
+interface ICardType {
+  children: React.ReactNode;
+  isLink?: boolean;
+  color:
+    | "tangerine"
+    | "grapefruit"
+    | "orange"
+    | "orangeLight"
+    | "pink"
+    | "plum"
+    | "purple"
+    | "lemon"
+    | "gray"
+    | "avocado"
+    | "grass"
+    | "blueLight";
+}
+
+const Card = ({ children, isLink, color }: ICardType) => {
+  const classComponent = `${styles.card} ${styles[color]}`;
+  const classLink = `${styles.isLink} ${classComponent}`;
+  const classesCard = isLink ? classLink : classComponent;
+  return <div className={classesCard}>{children}</div>;
+};
+
+export default Card;
